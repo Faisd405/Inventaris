fb54a71b722bd7fb7bb56a7be68e97f3eca7d1ff
faisd405 <faisd405@gmail.com>
2021-12-14 11:50:00 +0700

    datatables dengan bootstrap-vue, cetak pdf dan qr

--- resources/js/components/buku/index.vue
+++ resources/js/components/buku/index.vue
@@ -12,7 +12,62 @@
               >TAMBAH Data buku</router-link
             >
             <div class="table-responsive mt-2">
-              <b-table :items="buku" :fields="fields">
+                <b-row>
+                <b-col lg="6" class="my-1">
+                  <b-form-group
+                    label="Filter"
+                    label-for="filter-input"
+                    label-cols-sm="3"
+                    label-align-sm="right"
+                    label-size="sm"
+                    class="mb-0"
+                  >
+                    <b-input-group size="sm">
+                      <b-form-input
+                        id="filter-input"
+                        v-model="filter"
+                        type="search"
+                        placeholder="Type to Search"
+                      ></b-form-input>
+
+                      <b-input-group-append>
+                        <b-button :disabled="!filter" @click="filter = ''"
+                          >Clear</b-button
+                        >
+                      </b-input-group-append>
+                    </b-input-group>
+                  </b-form-group>
+                </b-col>
+                <b-col sm="5" md="6" class="my-1">
+                  <b-form-group
+                    label="Per page"
+                    label-for="per-page-select"
+                    label-cols-sm="6"
+                    label-cols-md="4"
+                    label-cols-lg="3"
+                    label-align-sm="right"
+                    label-size="sm"
+                    class="mb-0"
+                  >
+                    <b-form-select
+                      id="per-page-select"
+                      v-model="perPage"
+                      :options="pageOptions"
+                      size="sm"
+                    ></b-form-select>
+                  </b-form-group>
+                </b-col>
+              </b-row>
+              <b-table :items="buku" :fields="fields"
+                :sort-by.sync="sortBy"
+                striped
+                responsive="sm"
+                sort-icon-left
+                :filter="filter"
+                :filter-included-fields="filterOn"
+                @filtered="onFiltered"
+                :current-page="currentPage"
+                :per-page="perPage">
                   <template #cell(action)="data">
 
                       <router-link
@@ -52,7 +107,13 @@ export default {
         { key: "lokasi", label: "Lokasi" },
         { key: "action", label: "Action", sortable: false }
       ],
+        filter: null,
+        filterOn: [],
+        currentPage: 1,
+        perPage: 5,
+        pageOptions: [5, 15, 25, 50,{ value: 100, text: "Show a lot" }],
       buku: [],
+      sortBy: "id",
     };
   },
   created() {
@@ -61,7 +122,18 @@ export default {
       this.buku = response.data.buku;
     });
   },
+  computed: {
+    filterOn() {
+      return this.fields
+        .filter((field) => field.filterable)
+        .map((field) => field.key);
+    },
+  },
   methods: {
+    onFiltered(filteredItems) {
+        this.totalRows = filteredItems.length
+        this.currentPage = 1
+    },
     destroy(id) {
       let uri = `/api/buku/${id}`;
       this.axios.delete(uri).then((response) => {
